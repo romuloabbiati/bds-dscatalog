c89fd36deb1c19d0531d56f970724dad3669c0a3
romuloabbiati <rhabbiati@hotmail.com>
2023-08-19 09:11:59 +0100

    fishing off x-small layout

---
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const ButtonIcon = () => {
+    return (
+        <button className="btn btn-primary btn-icon">
+            <h6>BEGIN YOUR SEARCH HERE</h6>
+        </button>
+    );
+};
+
+export default ButtonIcon;
--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import ButtonIcon from 'components/ButtonIcon';
 import Navbar from 'components/Navbar';
 
 import './styles.css';
@@ -11,6 +12,8 @@ const Home = () => {
         <div className='home-card'>
             <div className='home-content-container'>
                 <h1>Find out the best product catalog</h1>
+                <p>We will help you to find the best products available in the market</p>
+                <ButtonIcon />
             </div>
             <div className='home-image-container'>
                 <MainImage />
