539ae85265e2015435f7761040a9add91b3de927
romuloabbiati <rhabbiati@hotmail.com>
2023-08-23 07:16:54 +0100

    showing more cards per line using bootstrap grid system

---
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const ProductPrice = () => {
+    return (
+        <div className='product-price-container'>
+            <span>£</span>
+            <h3>1345.67</h3>
+        </div>
+    );
+};
+
+export default ProductPrice;
