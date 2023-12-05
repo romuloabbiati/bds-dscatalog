f0522bd5f3ebd25af827b77cb43a2a03c3d8fb15
romuloabbiati <rhabbiati@hotmail.com>
2023-12-05 08:55:50 +0000

    Organising utils

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,9 +1,11 @@
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
-import { getTokenData, requestBackendLogin, saveAuthData } from 'util/requests';
+import { requestBackendLogin } from 'util/requests';
 import { useContext, useState } from 'react';
 import { AuthContext } from 'AuthContext';
+import { saveAuthData } from 'util/storage';
+import { getTokenData } from 'util/auth';
 
 import './styles.css';
 
@@ -17,22 +19,25 @@ type LocationState = {
 };
 
 const Login = () => {
-
   const location = useLocation<LocationState>();
 
-  const { from } = location.state || { from: { pathname: "/admin"}};
+  const { from } = location.state || { from: { pathname: '/admin' } };
 
   const { setAuthContextData } = useContext(AuthContext);
 
   const [hasError, setHasError] = useState(false);
 
-  const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const history = useHistory();
 
-  const onSubmit = (formData : FormData) => {
+  const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
-      .then(response => {
+      .then((response) => {
         saveAuthData(response.data);
         setHasError(false);
         setAuthContextData({
@@ -41,7 +46,7 @@ const Login = () => {
         });
         history.replace(from);
       })
-      .catch(error => {
+      .catch((error) => {
         setHasError(true);
         console.log('ERRO', error);
       });
@@ -50,39 +55,45 @@ const Login = () => {
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
-      { hasError && (
-        <div className="alert alert-danger">
-          Erro ao tentar efetuar o login
-        </div>
+      {hasError && (
+        <div className="alert alert-danger">Erro ao tentar efetuar o login</div>
       )}
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
-            {...register("username", {
+            {...register('username', {
               required: 'Campo obrigatório',
               pattern: {
                 value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: 'Email inválido'
-              }
+                message: 'Email inválido',
+              },
             })}
             type="text"
-            className={`form-control base-input ${errors.username ? 'is-invalid' : ''}`}
+            className={`form-control base-input ${
+              errors.username ? 'is-invalid' : ''
+            }`}
             placeholder="Email"
             name="username"
           />
-          <div className='invalid-feedback d-block'>{errors.username?.message}</div>
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
         </div>
         <div className="mb-2">
           <input
-            {...register("password", {
-              required: 'Campo obrigatório'
+            {...register('password', {
+              required: 'Campo obrigatório',
             })}
             type="password"
-            className={`form-control base-input ${errors.password ? 'is-invalid' : ''}`}
+            className={`form-control base-input ${
+              errors.password ? 'is-invalid' : ''
+            }`}
             placeholder="Password"
             name="password"
           />
-          <div className='invalid-feedback d-block'>{errors.password?.message}</div>
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
         </div>
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha
--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,123 +1,72 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 import history from './history';
-import jwtDecode from 'jwt-decode';
+import { getAuthData } from './storage';
 
-export type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
-
-export type TokenData = {
-    exp: number;
-    user_name: string;
-    authorities: Role[];
-};
-
-type LoginResponse = {
-    access_token: string;
-    token_type: string;
-    expires_in: number;
-    scope: string;
-    userFirstName: string;
-    userId: number;
-}
-
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
-
-const tokenKey = 'authData';
+export const BASE_URL =
+  process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
-const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123'
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
 
 type LoginData = {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 };
 
-export const requestBackendLogin = (loginData : LoginData) => {
+export const requestBackendLogin = (loginData: LoginData) => {
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+  };
 
-    const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET)
-    };
+  const data = qs.stringify({
+    ...loginData,
+    grant_type: 'password',
+  });
 
-    const data = qs.stringify({
-        ...loginData,
-        grant_type: 'password'
-    });
-
-    return axios({method: 'POST', baseURL: BASE_URL, url: '/oauth/token', data, headers});
+  return axios({
+    method: 'POST',
+    baseURL: BASE_URL,
+    url: '/oauth/token',
+    data,
+    headers,
+  });
 };
 
 export const requestBackend = (config: AxiosRequestConfig) => {
-    const headers = config.withCredentials ? {
+  const headers = config.withCredentials
+    ? {
         ...config.headers,
-        Authorization: "Bearer " + getAuthData().access_token
-    } : config.headers;
+        Authorization: 'Bearer ' + getAuthData().access_token,
+      }
+    : config.headers;
 
-    return axios({...config, baseURL: BASE_URL, headers});
-};
-
-export const saveAuthData = (obj : LoginResponse) => {
-    localStorage.setItem(tokenKey, JSON.stringify(obj));
-};
-
-export const getAuthData = () => {
-    const str = localStorage.getItem(tokenKey) ?? "{}";
-    return JSON.parse(str) as LoginResponse;
-};
-
-export const removeAuthData = () => {
-    localStorage.removeItem(tokenKey);
+  return axios({ ...config, baseURL: BASE_URL, headers });
 };
 
 // Add a request interceptor
-axios.interceptors.request.use(function (config) {
+axios.interceptors.request.use(
+  function (config) {
     //
     return config;
-  }, function (error) {
+  },
+  function (error) {
     //
     return Promise.reject(error);
-  });
+  }
+);
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
+axios.interceptors.response.use(
+  function (response) {
     //
     return response;
-  }, function (error) {
+  },
+  function (error) {
     if (error.response.status === 401) {
-        history.push('/admin/auth');
+      history.push('/admin/auth');
     }
     return Promise.reject(error);
-  });
-
-export const getTokenData = () : TokenData | undefined => {
-    try {
-        return jwtDecode(getAuthData().access_token);
-    } catch (error) {
-        return undefined;
-    }
-};
-
-export const isAuthenticated = () : Boolean => {
-    const tokenData = getTokenData();
-    return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
-};
-
-export const hasAnyRoles = (roles: Role[]) : boolean => {
-
-    if(roles.length === 0) {
-        return true;
-    }
-
-    const tokenData = getTokenData();
-
-    if(tokenData !== undefined) {
-        for(var i = 0; i < roles.length; i++) {
-            if(tokenData.authorities.includes(roles[i])) {
-                return true;
-            }
-        }
-        // roles.some(role => tokenData.authorities.includes(role));
-    }
-
-    return false;
-};
+  }
+);
